f8a2d6fd4f06b25dc892163679457306968e2174
jcamilovillah <villahjuancamilo@gmail.com>
2021-01-28 04:01:15 +0000

    config api files

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Show index"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def status():
+    """return status"""
+    return jsonify({"status": "OK"})
