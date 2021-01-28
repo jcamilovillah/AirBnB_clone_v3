ce6034fd8785fb43462e9a8c906a818be4e4b20c
jcamilovillah <villahjuancamilo@gmail.com>
2021-01-28 14:21:30 +0000

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """App file"""
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,9 @@ def close_storage(exception):
     """Storage close function"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", default="0.0.0.0")
